1ca987a7684f8632a7d267e23a2cbe9c177f4d8a
singoreS <singore.sissoko@gmail.com>
2023-09-20 19:01:50 +0200

    init

--- back/sql/select.sql
+++ back/sql/select.sql
@@ -11,17 +11,27 @@ ON
 
 
 -- récupérer les responsables liés aux établissements
-SELECT responsable.*, etablissement.*
-FROM Intergen.responsable
-JOIN Intergen.etablissement
-ON etablissement.id = responsable.etablissement_id;
+SELECT 
+    responsable.*, etablissement.*
+FROM 
+    Intergen.responsable
+JOIN 
+    Intergen.etablissement
+ON 
+    etablissement.id = responsable.etablissement_id;
 
 -- récupérer les activités d'un programme
-SELECT programme.*, activiter.*
-FROM Intergen.programme
-JOIN Intergen.activiter
-JOIN Intergen.programme_activiter
-ON programme_activiter.programme_id = programme.id
-AND programme_activiter.activiter_id = activiter.id
--- WHERE programme.id = 12
+SELECT 
+    programme.*, activiter.*
+FROM 
+    Intergen.programme
+JOIN 
+    Intergen.activiter
+JOIN 
+    Intergen.programme_activiter
+ON 
+    programme_activiter.programme_id = programme.id
+AND 
+    programme_activiter.activiter_id = activiter.id
+    -- WHERE programme.id = 12
 ;
--- front/assets/js/style.js
+++ front/assets/js/style.js
@@ -1,6 +1,1 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('mobile-menu');
-});
+let menu = document.get
